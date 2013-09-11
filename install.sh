2d5997d28c6b972fa38cc996af8beccea4952f43
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-09-11 15:08:14 +0200

    Improved install.sh

--- install.sh
+++ install.sh
@@ -5,23 +5,30 @@ CANONICAL_SRC=$(readlink -f $REL_SRC)
 DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
 
 
+# mail directories and mu directories
 mkdir -p ${HOME}/mail/{mail,mu}
 ln -f -s ${HOME}/mail/mail                    ${HOME}/.mail
 ln -f -s ${HOME}/mail/mu                      ${HOME}/.mu
 
 ln -f -s ${DIR}/mutt                          ${HOME}/.mutt
 
+# mutt-open and urlview
 ln -f -s ${DIR}/misc-tools/mutt-open          ${HOME}/bin/mutt-open
 ln -f -s ${DIR}/misc-tools/urlview            ${HOME}/.urlview
 
+# msmtp
 ln -f -s ${DIR}/msmtp.rc                      ${HOME}/.msmtprc
 
+# offlineimap
 ln -f -s ${DIR}/offlineimap/offlineimap.rc    ${HOME}/.offlineimaprc
 ln -f -s ${DIR}/offlineimap/offlineimap.py    ${HOME}/.offlineimap.py
 
+# goobook
 ln -f -s ${DIR}/goobook/goobook.rc            ${HOME}/.goobookrc
 chmod +x ${DIR}/goobook/goobook_getpass.sh
 ln -f -s ${DIR}/goobook/goobook_getpass.sh    ${HOME}/.goobook_getpass.sh
+touch ${DIR}/goobook/goobook_cache
+ln -f -s ${DIR}/goobook/goobook_cache         ${HOME}/.goobook_cache
 
 
 read -r -d '' PYCMD <<'EOF'
