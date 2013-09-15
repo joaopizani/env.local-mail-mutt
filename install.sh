4f53bd483545b201c4a01cb4b3ba519279b491c1
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-09-15 21:36:12 +0200

    pydelatt now comes from my personal fork

--- install.sh
+++ install.sh
@@ -7,34 +7,36 @@ DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
 
 # mail directories, mutt directories and mu directories
 mkdir -p ${HOME}/mail/mail
-ln -f -s ${HOME}/mail/mail                    ${HOME}/.mail
+ln -f -s ${HOME}/mail/mail                       ${HOME}/.mail
 mkdir -p ${DIR}/mu/{cache,xapian,results,log}
-ln -f -s ${DIR}/mu                            ${HOME}/.mu
+ln -f -s ${DIR}/mu                               ${HOME}/.mu
 
-ln -f -s ${DIR}/mutt                          ${HOME}/.mutt
+ln -f -s ${DIR}/mutt                             ${HOME}/.mutt
 mkdir -p ${HOME}/Downloads/mutt-attachments
 
 # mutt-open and urlview
-ln -f -s ${DIR}/misc-tools/mutt-open          ${HOME}/bin/mutt-open
-ln -f -s ${DIR}/misc-tools/urlview            ${HOME}/.urlview
+ln -f -s ${DIR}/misc-tools/mutt-open             ${HOME}/bin/mutt-open
+ln -f -s ${DIR}/misc-tools/urlview               ${HOME}/.urlview
 
 # msmtp
-ln -f -s ${DIR}/msmtp/msmtp.rc                ${HOME}/.msmtprc
-ln -f -s ${DIR}/msmtp/msmtp.log               ${HOME}/.msmtp.log
+ln -f -s ${DIR}/msmtp/msmtp.rc                   ${HOME}/.msmtprc
+ln -f -s ${DIR}/msmtp/msmtp.log                  ${HOME}/.msmtp.log
 
 # offlineimap
-ln -f -s ${DIR}/offlineimap/offlineimap.rc    ${HOME}/.offlineimaprc
-ln -f -s ${DIR}/offlineimap/offlineimap.py    ${HOME}/.offlineimap.py
+ln -f -s ${DIR}/offlineimap/offlineimap.rc       ${HOME}/.offlineimaprc
+ln -f -s ${DIR}/offlineimap/offlineimap.py       ${HOME}/.offlineimap.py
 
 # pydelatt, archival, etc.
-ln -s ${DIR}/misc-tools/pydelatt.py           ${HOME}/bin/pydelatt.py
+git clone https://gist.github.com/6572826.git    ${DIR}/misc-tools/pydelatt
+chmod +x ${DIR}/misc-tools/pydelatt/pydelatt.py
+ln -s ${DIR}/misc-tools/pydelatt/pydelatt.py     ${HOME}/bin/pydelatt.py
 
 # goobook
-ln -f -s ${DIR}/goobook/goobook.rc            ${HOME}/.goobookrc
+ln -f -s ${DIR}/goobook/goobook.rc               ${HOME}/.goobookrc
 chmod +x ${DIR}/goobook/goobook_getpass.sh
-ln -f -s ${DIR}/goobook/goobook_getpass.sh    ${HOME}/.goobook_getpass.sh
+ln -f -s ${DIR}/goobook/goobook_getpass.sh       ${HOME}/.goobook_getpass.sh
 touch ${DIR}/goobook/goobook_cache
-ln -f -s ${DIR}/goobook/goobook_cache         ${HOME}/.goobook_cache
+ln -f -s ${DIR}/goobook/goobook_cache            ${HOME}/.goobook_cache
 
 
 read -r -d '' PYCMD <<'EOF'
