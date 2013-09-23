88220b88c24778ec2287be6ecc7b843ddb8d6dec
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-09-23 18:31:07 +0200

    now install_news handles creation of nicely numbered links and mailboxes file

--- mutt/config-news/install_news.sh
+++ mutt/config-news/install_news.sh
@@ -7,16 +7,26 @@ DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
 mkdir -p "${HOME}/.cache/rss2maildir" "${HOME}/.config/rss2maildir"
 ln -f -s "${DIR}/feeds.json" "${HOME}/.config/rss2maildir/feeds.json"
 
-NEWSDIR_TRUE="${HOME}/comm/.news"
-NEWSDIR_LINKS="${HOME}/comm/mutt/news"
-NEWSDIR_GLOBAL="${HOME}/.news"
+NDIR_NAME="news"
+MUTTROOT="${HOME}/comm/mutt"
+NDIR_TRUE="${HOME}/comm/.news"
+NDIR_LINKS="${MUTTROOT}/${NDIR_NAME}"
+NDIR_GLOBAL="${HOME}/.news"
 
-mkdir -p ${NEWSDIR_TRUE} ${NEWSDIR_LINKS}
-ln -f -s ${NEWSDIR_TRUE} ${NEWSDIR_GLOBAL}
+mkdir -p ${NDIR_TRUE} ${NDIR_LINKS}
+ln -f -s ${NDIR_TRUE} ${NDIR_GLOBAL}
 
 maildir-feed
-for n in $(ls -A1 ${NEWSDIR_TRUE}); do
-    ln -f -s "${NEWSDIR_TRUE}/${n}" "${NEWSDIR_LINKS}/$(echo ${n} | sed 's/^.//')"
+
+CNT=0
+for n in $(ls -A1 ${NDIR_TRUE}); do
+    ln -f -s "${NDIR_TRUE}/${n}" "${NDIR_LINKS}/${CNT}_$(echo ${n} | sed 's/^.//')"
+    (( CNT++ ))
 done
 
-# Now, create the news-mailboxes file...
+# Now, create the news-mailboxes file
+NEWSBOXES_FILE="${DIR}/mailboxes-news"
+echo $(ls -A1 ${NDIR_LINKS}) >          ${NEWSBOXES_FILE}
+sed -i "s/ \(.\)/ +${NDIR_NAME}\/\1/g"  ${NEWSBOXES_FILE}
+sed -i "s/\(.\)/ +${NDIR_NAME}\/\1/"    ${NEWSBOXES_FILE}
+
