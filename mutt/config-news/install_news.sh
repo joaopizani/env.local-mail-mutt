5bc246238bb530b0b53c6a34b59b702a40cf33ce
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-11-01 08:56:19 +0100

    fixed install_news.sh to not create silly recursive link

--- mutt/config-news/install_news.sh
+++ mutt/config-news/install_news.sh
@@ -4,8 +4,8 @@ REL_SRC=${BASH_SOURCE[0]}
 CANONICAL_SRC=$(readlink -f $REL_SRC)
 DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
 
-#mkdir -p "${HOME}/.cache/rss2maildir" "${HOME}/.config/rss2maildir"
-#ln -f -s "${DIR}/feeds.json" "${HOME}/.config/rss2maildir/feeds.json"
+mkdir -p "${HOME}/.cache/rss2maildir" "${HOME}/.config/rss2maildir"
+ln -f -s "${DIR}/feeds.json" "${HOME}/.config/rss2maildir/feeds.json"
 
 NDIR_NAME="news"
 MUTTROOT="${HOME}/comm/mutt"
@@ -13,16 +13,16 @@ NDIR_TRUE="${HOME}/comm/.news"
 NDIR_LINKS="${MUTTROOT}/${NDIR_NAME}"
 NDIR_GLOBAL="${HOME}/.news"
 
-#mkdir -p ${NDIR_TRUE} ${NDIR_LINKS}
-#ln -f -s ${NDIR_TRUE} ${NDIR_GLOBAL}
+mkdir -p ${NDIR_TRUE} ${NDIR_LINKS}
+ln -f -s -T ${NDIR_TRUE} ${NDIR_GLOBAL}
 
-#maildir-feed ${NDIR_GLOBAL}
+maildir-feed ${NDIR_GLOBAL}
 
-#CNT=0
-#for n in $(ls -A1 ${NDIR_TRUE}); do
-#    ln -f -s "${NDIR_TRUE}/${n}" "${NDIR_LINKS}/${CNT}_$(echo ${n} | sed 's/^.//')"
-#    (( CNT++ ))
-#done
+CNT=0  # numbered links only work now from 0 until 9, TODO two-digit numbers
+for n in $(ls -A1 ${NDIR_TRUE}); do
+    ln -f -s "${NDIR_TRUE}/${n}" "${NDIR_LINKS}/${CNT}_$(echo ${n} | sed 's/^.//')"
+    (( CNT++ ))
+done
 
 
 # Now, create the news-mailboxes file and mutt-news.rc
