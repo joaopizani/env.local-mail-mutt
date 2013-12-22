5aa7a105ba29623e852f37403bfc0d6f38faf113
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-12-22 14:37:05 -0200

    factored msmtp.rc generation into separate script for username segregation

---
+++ msmtp/msmtprc-generate.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+REL_SRC=${BASH_SOURCE[0]}
+CANONICAL_SRC=$(readlink -f $REL_SRC)
+DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
+
+EMAIL_ADDRESS_FILE_LINK="${HOME}/.localmail-username-gmail"
+EMAIL_ADDRESS=$(cat ${EMAIL_ADDRESS_FILE_LINK})
+
+cat <<EOF > "${DIR}/msmtp.rc"
+account gmail
+host smtp.gmail.com
+port 587
+protocol smtp
+auth on
+from ${EMAIL_ADDRESS}
+user ${EMAIL_ADDRESS}
+tls on
+tls_starttls on
+tls_trust_file /etc/ssl/certs/ca-certificates.crt
+
+account default : gmail
+logfile ~/.msmtp.log
+EOF
+
