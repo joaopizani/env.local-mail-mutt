170ccf841d5738588d3c9b80811dbcd0fa78fd39
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-12-22 19:23:59 -0200

    goobook configs using localmail-custom dir path

--- goobook/goobook_getpass.sh
+++ goobook/goobook_getpass.sh
@@ -3,7 +3,7 @@
 import keyring
 import os
 
-with open(os.path.expanduser('~/.localmail-username-gmail'), 'r') as localmail_username_file:
+with open(os.path.expanduser('~/.localmail-custom/username-gmail'), 'r') as localmail_username_file:
     localmail_username = localmail_username_file.read()
 
 print keyring.get_password("localmail-gmail", localmail_username)
