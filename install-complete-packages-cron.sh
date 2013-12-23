a1c0c2b47aab54aa5a0c5f626f9a1e3aeb714c1b
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-12-23 16:39:29 -0200

    added quoting to install-complete

--- install-complete-packages-cron.sh
+++ install-complete-packages-cron.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+REL_SRC=${BASH_SOURCE[0]}
+CANONICAL_SRC=$(readlink -f $REL_SRC)
+DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
+
 echo "Installing prerequisite packages"
 sudo apt-get install $(cat ubuntu-packages-needed)
 
 cat <(crontab -l) <(cron-jobs) | crontab -
 
-./install-basic.sh
+"${DIR}/install-basic.sh"
+
