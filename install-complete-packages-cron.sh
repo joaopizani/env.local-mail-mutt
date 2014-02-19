81a38fff1709aff0aaf8a8a3326277ad2ed5d589
João Paulo Pizani Flor <joaopizani@gmail.com>
2014-02-19 23:03:01 +0100

    corrected typo in install-complete, made also cron-jobs use offlineimap every hour (30min was not working)

--- install-complete-packages-cron.sh
+++ install-complete-packages-cron.sh
@@ -5,7 +5,7 @@ DIR="$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
 echo "Installing prerequisite packages"
 sudo apt-get install $(cat ubuntu-packages-needed)
 
-cat <(crontab -l) <(cron-jobs) | crontab -
+cat <(crontab -l) "cron-jobs" | crontab -
 
 "${DIR}/install-basic.sh"
 
