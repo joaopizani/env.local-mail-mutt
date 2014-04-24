36e33fd7e4e7edcdae3cc28ff1f6f8f653a8695c
João Paulo Pizani Flor <joaopizani@gmail.com>
2014-04-24 17:50:36 +0200

    install script of the msmtp subsec also sets msmtp-gnome-keyring password now

--- msmtp/install.sh
+++ msmtp/install.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-REL_SRC=${BASH_SOURCE[0]}
-CANONICAL_SRC=$(readlink -f $REL_SRC)
-DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
+DIR="$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
+
+EMAIL_ADDRESS_FILE_LINK="${HOME}/.localmail-custom/username-gmail"
+EMAIL_ADDRESS="$(cat "${EMAIL_ADDRESS_FILE_LINK}")"
 
 
 "${DIR}/msmtprc-generate.sh"
 ln -f -s -n "${DIR}/msmtp.rc"     "${HOME}/.msmtprc"
-
 ln -f -s -n "${DIR}/msmtp.log"    "${HOME}/.msmtp.log"
+
+SETTERSCRIPT="msmtp-gnome-tool.py"
+cp "/usr/share/doc/msmtp/scripts/msmtp-gnome-tool.py" "${SETTERSCRIPT}"
+chmod +x "${SETTERSCRIPT}"
+./${SETTERSCRIPT} -s --username "${EMAIL_ADDRESS}" --server "smtp.gmail.com"
+rm "${SETTERSCRIPT}"
 
